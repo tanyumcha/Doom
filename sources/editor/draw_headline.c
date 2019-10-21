b29ef6e554a67e6341bf1a9f16a84ec353519953
tanyumcha <boolala8@yandex.ru>
2019-10-21 14:53:12 +0300

    [ADD] bottomline

--- sources/editor/draw_headline.c
+++ sources/editor/draw_headline.c
@@ -6,7 +6,7 @@
 /*   By: eharrag- <eharrag-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:23:36 by eharrag-          #+#    #+#             */
-/*   Updated: 2019/10/21 14:48:08 by eharrag-         ###   ########.fr       */
+/*   Updated: 2019/10/21 14:52:47 by eharrag-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	draw_headline(t_sdl *sdl)
 	headline.h = 50;
 	SDL_SetRenderDrawColor(sdl->renderer, 20, 20, 40, 0);
 	draw_text(sdl, " Remove last command - BACKSPACE",
-	headline, *(create_sdl_color(204, 153, 255, 255)));
+		headline, *(create_sdl_color(204, 153, 255, 255)));
 	bottomline.x = 94;
 	bottomline.y = SIZE_WIN_Y - headline.y - headline.h;
 	bottomline.w = (int)SIZE_WIN_X * 0.8 - headline.x * 2;
