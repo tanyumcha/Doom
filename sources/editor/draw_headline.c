f2c90801e9d832b07577f50f06c3ee98f1b4fd42
tanyumcha <boolala8@yandex.ru>
2019-10-21 14:49:35 +0300

    [ADD] bottomline

--- sources/editor/draw_headline.c
+++ sources/editor/draw_headline.c
@@ -6,7 +6,7 @@
 /*   By: eharrag- <eharrag-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:23:36 by eharrag-          #+#    #+#             */
-/*   Updated: 2019/10/21 14:36:47 by eharrag-         ###   ########.fr       */
+/*   Updated: 2019/10/21 14:48:08 by eharrag-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 void	draw_headline(t_sdl *sdl)
 {
 	SDL_Rect	headline;
+	SDL_Rect	bottomline;
 
 	headline.x = 94;
 	headline.y = 25;
 	headline.w = (int)SIZE_WIN_X * 0.8 - headline.x * 2;
 	headline.h = 50;
 	SDL_SetRenderDrawColor(sdl->renderer, 20, 20, 40, 0);
-	SDL_RenderFillRect(sdl->renderer, &headline);
+	draw_text(sdl, " Remove last command - BACKSPACE",
+	headline, *(create_sdl_color(204, 153, 255, 255)));
+	bottomline.x = 94;
+	bottomline.y = SIZE_WIN_Y - headline.y - headline.h;
+	bottomline.w = (int)SIZE_WIN_X * 0.8 - headline.x * 2;
+	bottomline.h = headline.h;
 	draw_text(sdl,
-		" Rm last command - BACKSPACE || Rm player - DELETE || Full reset - R",
-		headline, *(create_sdl_color(204, 153, 255, 255)));
+		" Remove player - DELETE                  Full reset - R",
+		bottomline, *(create_sdl_color(204, 153, 255, 255)));
 }
